6da474548934944ff6fa81b56569c1d3e7625e72
Amal Chelbi <108265995+A-chelbi@users.noreply.github.com>
2022-09-15 13:44:08 +0100

    update last part

--- readme.js
+++ readme.js
@@ -33,4 +33,10 @@ I am a web developer focusing on Front-end for PWA with a strong will to learn a
 
 ![](https://github-readme-streak-stats.herokuapp.com/?user=A-chelbi&theme=dark&hide_border=true)<br/>
 ![](https://github-readme-stats.vercel.app/api/top-langs/?username=A-chelbi&theme=dark&hide_border=true&include_all_commits=false&count_private=true&layout=compact)
+
+---
+
+<p align="center">This <i>README</i> file is generated <b>by Gabot ❤️</b>!</br>Last update on <#today_date><br /></p>
+
+<p align="center"> <a href="https://medium.com/@th.guibert/how-to-create-a-self-updating-readme-md-for-your-github-profile-f8b05744ca91">Create your own here!</a> </p>
 `;
