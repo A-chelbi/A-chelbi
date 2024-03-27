cf23d079dc50d552ebae93118337fbfa8dcf4e7f
Amal chelbi <amal.chelbi@ealpha4tech.com>
2024-03-27 11:32:55 +0100

    Update actions

--- readme.js
+++ readme.js
@@ -32,6 +32,4 @@ I am a web developer focusing on Front-end for PWA with a strong will to learn a
 ---
 
 <p align="center">This <i>README</i> file is generated <b>by Gabot ❤️</b>!</br>Last update on <#today_date><br /></p>
-
-<p align="center"> <a href="https://medium.com/@th.guibert/how-to-create-a-self-updating-readme-md-for-your-github-profile-f8b05744ca91">Create your own here!</a> </p>
 `;
